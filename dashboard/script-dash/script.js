21427a30af6d13702068af93c5a9906221970329
Oevem Gonçalves <oevemrodrigues2@gmail.com>
2025-05-10 20:46:33 -0300

    ajuste na seta. retirada de alguns elementos. ajuste do scroll

--- dashboard/script-dash/script.js
+++ dashboard/script-dash/script.js
@@ -1,14 +1,19 @@
 function novaPasta() {
     const modal = document.getElementById('modal');
     modal.classList.remove('hidden');
+    document.body.classList.add('modal-open'); // <-- trava o scroll
+
     setTimeout(() => {
         modal.classList.add('active');
+        document.getElementById('nomePasta').focus();
     }, 10); // Pequeno delay para o transition funcionar
 }
 
 function fecharModal() {
     const modal = document.getElementById('modal');
     modal.classList.remove('active');
+    document.body.classList.remove('modal-open') // <-- destrava o scroll
+
     setTimeout(() => {
         modal.classList.add('hidden');
     }, 300); // Tempo igual ao transition de 0.3s
@@ -25,5 +30,3 @@ function toggleSidebar() {
     ? '<i class="fa-regular fa-square-caret-left"></i>'
     : '<i class="fa-regular fa-square-caret-right"></i>';
 }
-
-  
