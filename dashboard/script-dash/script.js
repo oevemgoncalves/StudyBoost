7cc3db0b25706cff6d04b385d3f0be080b84a373
Oevem Gonçalves <oevemrodrigues2@gmail.com>
2025-05-10 23:03:54 -0300

    adicionando a criação de pasta no JS

--- dashboard/script-dash/script.js
+++ dashboard/script-dash/script.js
@@ -1,3 +1,15 @@
+//seta da sidebar
+const sidebar = document.querySelector('.sidebar');
+const arrow = document.getElementById('toggleArrow');
+
+function toggleSidebar() {
+  sidebar.classList.toggle('open');
+
+  arrow.innerHTML = sidebar.classList.contains('open')
+    ? '<i class="fa-regular fa-square-caret-left"></i>'
+    : '<i class="fa-regular fa-square-caret-right"></i>';
+}
+
 function novaPasta() {
     const modal = document.getElementById('modal');
     modal.classList.remove('hidden');
@@ -19,14 +31,47 @@ function fecharModal() {
     }, 300); // Tempo igual ao transition de 0.3s
 }
 
-//seta da sidebar
-const sidebar = document.querySelector('.sidebar');
-const arrow = document.getElementById('toggleArrow');
+//crição da pasta
+function criarPasta() {
+    let folderName = document.getElementById("pastaNome").value;
 
-function toggleSidebar() {
-  sidebar.classList.toggle('open');
+    if (folderName.trim() === "") {
+        alert("Por favor, insira um nome para a pasta");
+        return;
+    }
 
-  arrow.innerHTML = sidebar.classList.contains('open')
-    ? '<i class="fa-regular fa-square-caret-left"></i>'
-    : '<i class="fa-regular fa-square-caret-right"></i>';
+    let folder = document.createElement("div") //criando div
+    folder.className = "folder"; //add classe a pasta
+
+    let folderIcon = document.createElement("i");
+    folderIcon.className = "fa-regular fa-folder-open";
+
+    let folderTitle = document.createElement("span");
+    folderTitle.textContent = folderName;
+
+    folder.appendChild(folderIcon);
+    folder.appendChild(folderTitle);
+    document.getElementById("folder-container").appendChild(folder);
+
+    //criando o conteúdo da pasta no main
+    let folderContent = document.createElement("div");
+    folderContent.className = "folder-content";
+    folderContent.innerHTML = `<h3>${folderName}</h3><p>Adicionar Texto:</p> <textarea placeholder="Escreva alog..."></textarea>`;
+
+    folder.addEventListener("click", function() {
+        //atualizar o conteúdo no main trocar ícones
+        document.getElementById("folder__content__container").innerHTML = folderContent.innerHTML;
+
+        document.querySelectorAll(".folder i").forEach(icon => {icon.className = "fa-regular fa-folder-closed";});
+
+        folderIcon.className = "fa-regular fa-folder-open";
+
+    });
+
+    //limpa o campo e fecha o moda
+    document.getElementById("pastaNome").value = "";
+
+    document.getElementById("folder__content__container").innerHTML = folderContent.innerHTML;
+
+    console.log("Pasta criada:", folderName);
 }
