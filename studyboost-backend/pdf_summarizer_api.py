b559441f9da2d8e9789daacb2180e4f287a7ba03
Oevem Gonçalves <oevemrodrigues2@gmail.com>
2025-06-13 10:41:38 -0300

    remoção

--- studyboost-backend/pdf_summarizer_api.py
+++ studyboost-backend/pdf_summarizer_api.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"]  # Adicione esta linha
+    #expose_headers=["*"]  # Adicione esta linha
 )
 
 @app.get("/")
